10c98895964f6fecc10467d75b83d921908e8a86
Lin Qun <jinasonlin@gmail.com>
2019-06-14 11:14:07 +0800

    comparise rollup & webpack

---
+++ glove.umd.js
@@ -0,0 +1,22 @@
+module.exports = {
+  type: 'lib',
+  assetPath: 'dist',
+  entries: {
+    index: {
+      entry: './src/ContextState.jsx',
+    },
+  },
+  plugins: ['react'],
+  scriptSourceMap: false,
+  injectConfig(gConfig) {
+    gConfig.set('JavaScriptAssets', {
+      filename: 'glove/[name].umd.js',
+    });
+  },
+  applyConfig({ config }) {
+    config.externals = {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    };
+  }
+};
